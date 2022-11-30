06af0b2b0a8089955f62bd320e24ff01234f019b
ReshCuevas <cadavericsour@gmail.com>
2022-11-30 09:37:17 -0600

    Add files via upload

---
+++ db/mongodb-connect.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+
+const dbUrl = "mongodb+srv://adminman:justapassword@data.ipbv33b.mongodb.net/?retryWrites=true&w=majority";
+mongoose.connect(dbUrl,{
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+}).then(() => console.log("Ahoy sailor"))
+  .catch((err)=>console.log("Something's wrong matey", err));
+
+module.exports = mongoose;
